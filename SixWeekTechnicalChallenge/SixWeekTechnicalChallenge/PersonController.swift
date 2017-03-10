a8e43a3382931348362694150d19c98109231893
devonfaraday <christianmcmullin@gmail.com>
2017-03-10 09:19:06 -0700

    crud created

--- SixWeekTechnicalChallenge/SixWeekTechnicalChallenge/PersonController.swift
+++ SixWeekTechnicalChallenge/SixWeekTechnicalChallenge/PersonController.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+import CoreData
+
+class PersonController {
+    
+    static let shared = PersonController()
+    var people = [Person]() {
+        didSet {
+            fetchFromPersistentStore()
+        }
+    }
+    
+    
+    func createPerson(withName name: String) {
+        let _ = Person(name: name)
+        saveToPersistentStore()
+    }
+    
+    
+    func fetchFromPersistentStore() {
+        let request: NSFetchRequest<Person> = Person.fetchRequest()
+        self.people = (try? CoreDataStack.context.fetch(request)) ?? []
+        
+    }
+    
+    func saveToPersistentStore() {
+        let moc = CoreDataStack.context
+        do {
+            try moc.save()
+        } catch let error {
+            NSLog("Error: \(error.localizedDescription)\n Error Saving")
+        }
+        print("Saving was successful")
+    }
+}
